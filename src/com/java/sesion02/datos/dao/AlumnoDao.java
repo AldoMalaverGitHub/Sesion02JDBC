3bdaafb862f15d2e3e92f4295f1d7b4e977380d9
aldoMalaverGitHub <almalaver@gmail.com>
2019-02-20 21:46:03 -0500

    second commit

--- src/com/java/sesion02/datos/dao/AlumnoDao.java
+++ src/com/java/sesion02/datos/dao/AlumnoDao.java
@@ -1,6 +1,16 @@
 package com.java.sesion02.datos.dao;
 
+import java.util.List;
+
+import com.java.sesion02.datos.entidades.Alumno;
+
 public interface AlumnoDao {
 
+	List<Alumno> listarAlumnos();
 	
+	int guardarAlumno(Alumno alumno);
+	
+	int actualizarAlumno(Alumno alumno);
+	
+	int eliminarAlumno(int id);
 }
